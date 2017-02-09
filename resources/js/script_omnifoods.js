b7ce75c09fad08a121d6c967c3fb0ecbc8b37ca5
Jeff <jgomez630@aol.com>
2017-02-09 13:41:20 -0800

    added scrolling features

--- resources/js/script_omnifoods.js
+++ resources/js/script_omnifoods.js
@@ -1,3 +1,5 @@
+
+/*For Sticky Nav*/
 $(document).ready(function() {
   
   $('.js--section-features').waypoint(function(direction) {
@@ -8,6 +10,17 @@ $(document).ready(function() {
     }
   });
 });
+
+/*Scroll on buttons*/
+$('.js--scroll-to-plans').click(function(){
+  $('html,body').animate({scrollTop: $('.js--section-plans').offset().top},1000);
+});
+
+$('.js--scroll-to-start').click(function(){
+  $('html,body').animate({scrollTop: $('.js--section-features').offset().top},1000);
+});
+
+/*Navigation scroll*/
 
 
 
